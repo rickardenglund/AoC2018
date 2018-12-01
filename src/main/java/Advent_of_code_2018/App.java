bc81b623d2fccc93c4769dc3c5fa4259e6ecaf6d
Rickard Englund <rickard.englund@gmail.com>
2018-12-01 09:36:54 +0100

    Solve D1

--- src/main/java/Advent_of_code_2018/App.java
+++ src/main/java/Advent_of_code_2018/App.java
@@ -3,12 +3,16 @@
  */
 package Advent_of_code_2018;
 
+import Advent_of_code_2018.days.Day;
+import Advent_of_code_2018.days.day1.Day1;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
+    public static void main(String[] args) {
+        Day day = new Day1();
+
+        System.out.println("### Day " + day.getDay() + " ###");
+        System.out.println("P1: " + day.getResultP1(day.getInput1()));
+        System.out.println("P2: " + day.getResultP2(day.getInput2()));
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
 }
---
+++ src/main/java/Advent_of_code_2018/days/Day.java
@@ -0,0 +1,10 @@
+package Advent_of_code_2018.days;
+
+public interface Day {
+    Object getResultP1(String input);
+    Object getResultP2(String input);
+    int getDay();
+    String getInput1();
+    String getInput2();
+
+}
