46d23918d6bb79b5f35d99d2687640e8b8684c06
Rickard Englund <rickard.englund@gmail.com>
2018-12-11 20:22:58 +0100

    add gradle dayGenerator

---
+++ src/template/java/DayXX.java
@@ -0,0 +1,20 @@
+package Advent_of_code_2018.days.dayXX;
+
+import Advent_of_code_2018.days.Day;
+
+public class DayXX implements Day {
+    @Override
+    public Object getResultP1(String input) {
+        return null;
+    }
+
+    @Override
+    public Object getResultP2(String input) {
+        return null;
+    }
+
+    @Override
+    public int getDay() {
+        return XX;
+    }
+}
---
+++ src/template/test/DayXXTest.java
@@ -0,0 +1,17 @@
+package Advent_of_code_2018.days.dayXX;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DayXXTest {
+
+    @Test
+    public void p1() {
+        String expected = null;
+        String input = "";
+        var sut = new DayXX();
+        assertEquals(expected, sut.getResultP1(input));
+    }
+
+}
